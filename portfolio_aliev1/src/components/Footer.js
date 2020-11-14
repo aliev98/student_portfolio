a2925c43b90e34eafe791f0dcc57f2ab21d9e0d7
Ali Elias <kuba-98@hotmail.com>
2020-11-15 00:15:31 +0100

    update

--- portfolio_aliev1/src/components/Footer.js
+++ portfolio_aliev1/src/components/Footer.js
@@ -1,28 +1,33 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
- import styled from 'styled-components'
+import styled from 'styled-components'
 
 const FooterStyling = styled.footer `
-height: 9rem;
+  height: 9rem;
   background: #222;
   text-align: center;
   display: grid;
   place-items: center;
+  
   h4 {
     margin-top: 0.5rem;
     color: ${({ theme }) => theme.colors.white};
     font-weight: normal;
     text-transform: uppercase;
+
     span {
       color: ${({ theme }) => theme.colors.primary5};
     }
+    
   }
 `
+
 
 const Footer = () => {
   return (
       //  <footer className="footer"> 
          <FooterStyling>
+
       <div className="footer-content">
         <SocialLinks styleClass="footer-links"></SocialLinks>
         <h4>
@@ -30,6 +35,7 @@ const Footer = () => {
           <span>FrontendMAGIC</span> | all rights reserved
         </h4>
       </div>
+
        </FooterStyling>
     //  </footer>
   )
