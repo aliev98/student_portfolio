f329ede4a1584ccf8d19e1683f38df783ce7774f
Ali Elias <kuba-98@hotmail.com>
2020-11-22 21:17:44 +0100

    build

--- portfolio_aliev1/src/components/Layout.js
+++ portfolio_aliev1/src/components/Layout.js
@@ -36,7 +36,7 @@ import Footer from "./Footer"
   This is the main container-component to buid our pages with
   Every page will have as Nav, Footer and a Sidebar (mobile view). 
   We create local state and toggle functionality for the sidebar. 
-*/  
+*/
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false)
   const toggleSidebar = () => {
